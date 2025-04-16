570b64a7050b636158bc9d016ee4de87f0203dc9
kimmjiinn <nankimminjin@gmail.com>
2025-04-17 00:11:32 +0900

    style: enemyAI
    AI 클래스의 파일이름을 EnemyAI로 바꿈(안의 클래스명은 그대로)

---
+++ 250304_WinAPI/EnemyAI.cpp
@@ -0,0 +1,138 @@
+﻿#include "EnemyAI.h"
+
+
+
+
+HRESULT AI::Init()
+{
+    if (enemy)
+    {
+        enemyInFov = false;
+        justAlerted = false;
+
+        aiStatus = "None";
+        vecAiSelector.resize(2);
+        vecAiSelector[0] = new Hunting;
+        vecAiSelector[1] = new Wandering;
+
+        aiCurrState = vecAiSelector[1];
+        return S_OK;
+    }
+
+    return E_FAIL;
+}
+
+void AI::Release()
+{
+}
+
+void AI::Update()
+{
+    // 같은 스테이트면 유지시키다가 바뀌어야 하는 순간에 바꿈
+}
+
+void AI::Render(HDC hdc)
+{
+}
+
+bool AI::canAttack(Enemy* enemy)
+{
+    // 이동할 수 있는 만큼 갔을 때 플레이어 타일까지 간다면 공격할 수 있다. 즉, 이동타일 범위 안에 플레이어가 있으면 공격 가능
+
+    return false;   // 공격할 수 없다.
+}
+
+bool AI::canSee()   // Fov
+{
+
+    return false;   // 볼 수 없다.
+}
+
+bool AI::act(bool enemyInFov, bool justAlerted)     // 이 fov가 화면 fov인가..
+{
+
+
+    return false;
+}
+
+AI::~AI()
+{
+}
+
+//
+HRESULT Acting::Init()
+{
+    return E_NOTIMPL;
+}
+
+void Acting::Release()
+{
+}
+
+void Acting::Update()
+{
+}
+
+void Acting::Render(HDC hdc)
+{
+}
+
+// Hunting
+HRESULT Hunting::Init()
+{
+    Super::Init();
+    return E_NOTIMPL;
+}
+
+void Hunting::Release()
+{
+    Super::Release();
+}
+
+void Hunting::Update()
+{
+    Super::Update();
+}
+
+void Hunting::Render(HDC hdc)
+{
+    Super::Render(hdc);
+}
+
+bool Hunting::act(string& status)
+{
+
+    return false;
+}
+
+// Wandering
+HRESULT Wandering::Init()
+{
+    Super::Init();
+    return E_NOTIMPL;
+}
+
+void Wandering::Release()
+{
+    Super::Release();
+}
+
+void Wandering::Update()
+{
+    Super::Update();
+}
+
+void Wandering::Render(HDC hdc)
+{
+    Super::Render(hdc);
+}
+
+bool Wandering::act(string& status)
+{
+   // 아무지점이나 목표로 잡아서 움직임
+
+
+    return true;
+}
+
+
---
+++ 250304_WinAPI/EnemyAI.h
@@ -0,0 +1,96 @@
+﻿#pragma once
+#include "config.h"
+
+// 위치, 애니메이션 정도 바뀔듯
+class Enemy;
+class Acting;
+
+struct Node
+{
+	POINT pos;      // center값으로 보정하기.
+	float costFromStart;
+	float costToGoal;
+	float totalCost;
+
+	Node* parentTile;
+
+	// pos 범위: (ASTAR_TILE_SIZE / 2) ~ ASTAR_TILE_COUNT * ASTAR_TILE_SIZE + (ASTAR_TILE_SIZE / 2)
+};
+
+class AI
+{
+private:
+	bool enemyInFov;
+	bool justAlerted;
+	string aiStatus;
+	vector<Acting*> vecAiSelector;
+	Acting* aiCurrState;
+	Enemy* enemy;
+public:
+	AI(Enemy* enemy) : enemy(enemy) {};
+
+	HRESULT Init();
+	void Release();
+	void Update();
+	void Render(HDC hdc);
+
+	bool canAttack(Enemy* enemy);
+	bool canSee();
+	bool act(bool enemyInFov, bool justAlerted);		// execution, leaf
+//이 act에서 두 bool값으로 어떤 행동을 취할지 결정(시야 안/인식)
+	~AI();
+};
+
+class Acting
+{
+public:
+	virtual HRESULT Init();
+	virtual void Release();
+	virtual void Update() ;
+	virtual void Render(HDC hdc);
+
+	Acting() {};
+	virtual bool act(string &status) = 0;
+	virtual ~Acting() {};
+};
+
+
+class Hunting: public Acting
+{
+	// 이미지 애니메이션?'
+	// 타게을 따라간다
+	// 타겟값은 맵에서 받아온다.
+	
+public:
+	using Super = Acting;
+
+	virtual HRESULT Init() override;
+	virtual void Release() override;
+	virtual void Update() override;
+	virtual void Render(HDC hdc) override;
+
+	Hunting() {};
+	virtual bool act(string& status) override;
+	virtual ~Hunting() {};
+};
+
+class Wandering: public Acting
+{
+	// 주변에 타겟 없음
+	// 돌아다닌다
+	// 내비게이션 필요?
+public:
+	using Super = Acting;
+
+	virtual HRESULT Init() override;
+	virtual void Release() override;
+	virtual void Update() override;
+	virtual void Render(HDC hdc) override;
+
+	Wandering() {};
+	virtual bool act(string& status) override;
+	virtual ~Wandering() {};
+};
+
+// Status로 상태 받아옴-failure, succeeded, running
+// failure이면 종료, succeeded면 끝난거니까 조건 확인, running은 기존ㄱ거 반보.
