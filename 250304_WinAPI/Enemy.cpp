de6ba0739973b8c4e53c78d6f30dac2f85768020
kimmjiinn <nankimminjin@gmail.com>
2025-04-16 10:41:45 +0900

    feat: enemy
    재훈님 player 기반으로 만듦

---
+++ 250304_WinAPI/Enemy.cpp
@@ -0,0 +1,81 @@
+﻿#include "Enemy.h"
+#include "Image.h"
+#include "ImageManager.h"
+#include "TimerManager.h"
+
+
+Enemy::Enemy(POINT pos)
+{
+    this->pos = pos;
+}
+
+Enemy::~Enemy()
+{
+}
+
+HRESULT Enemy::Init()
+{
+	Super::Init();
+	currImage = ImageManager::GetInstance()->AddImage(
+		"rat_Idle", L"Image/Rat_Idle.bmp", 16*4, 16, 4, 1, true, RGB(255, 0, 255));
+	return S_OK;
+}
+
+void Enemy::Release()
+{
+	Super::Release();
+}
+
+void Enemy::Update()
+{
+	Super::Update();
+
+	float deltaTime = TimerManager::GetInstance()->GetDeltaTime();
+
+	currTick += deltaTime;
+
+	if (currTick > 0.1f)
+	{
+		currTick = 0;
+		currAnimFrame++;
+		if (currAnimFrame >= currImage->GetMaxFrameX())
+		{
+			currAnimFrame = 0;
+		}
+	}
+}
+
+void Enemy::Render(HDC hdc)
+{
+	Super::Render(hdc);
+	if (currImage)
+		currImage->FrameRender(hdc, pos.x, pos.y, currAnimFrame, 0);
+}
+
+void Enemy::SetState(ActState state)
+{
+	if (state == state)
+		return;
+
+	this->state = state;
+	UpdateAnimation();
+}
+
+void Enemy::SetDir(Dir dir)
+{
+	this->dir = dir;
+	UpdateAnimation();
+}
+
+void Enemy::UpdateAnimation()
+{
+	switch (state)
+	{
+	case ActState::IDLE:
+		break;
+	case ActState::MOVE:
+		break;
+	case ActState::ATTACK:
+		break;
+	}
+}
---
+++ 250304_WinAPI/Enemy.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include "Actor.h"
+#include "config.h"
+
+class Image;
+class Enemy: public Actor
+{
+public:
+	using Super = Actor;
+	Enemy(POINT pos);
+	virtual ~Enemy();
+
+	virtual HRESULT Init() override;
+	virtual void Release();
+	virtual void Update();
+	virtual void Render(HDC hdc);
+
+	void SetState(ActState state);
+	void SetDir(Dir dir);
+	void SetPos(POINT pos) { this->pos = pos; }
+
+	void UpdateAnimation();
+private:
+	Image* imageIdle[4] = {};
+	Image* imageMove[4] = {};
+	Image* imageAttack[4] = {};
+
+	int speed = {};
+	Dir dir = DIR_DOWN;
+	ActState state = ActState::IDLE;
+	bool keyPressed = false;
+};
