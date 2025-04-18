e062ec8390550f366cb1c19838d593d68e5dd1ea
kimmjiinn <nankimminjin@gmail.com>
2025-04-18 18:20:13 +0900

    feat: Entity

--- 250304_WinAPI/Entity.h
+++ 250304_WinAPI/Entity.h
@@ -10,7 +10,7 @@ public:
 	int graphicID;
 
 public:
-	void SetPosition(int x, int y);
+	void SetPosition(int x, int y) { pos.x = x; pos.y = y; };
 	//POINT GetPosition() { return { x, y }; };	//원래 형태: Vector2 GetPosition();
 	POINT GetPosition() { return pos; }
 };
