18f964657e896a660b1041aac425ee75db4b1c98
kimmjiinn <nankimminjin@gmail.com>
2025-04-18 18:19:09 +0900

    feat: Entity class
    Entity 클래스 추가

---
+++ 250304_WinAPI/Entity.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "config.h"
+
+class Entity
+{
+public:
+	//int x;
+	//int y;
+	POINT pos;
+	int graphicID;
+
+public:
+	void SetPosition(int x, int y);
+	//POINT GetPosition() { return { x, y }; };	//원래 형태: Vector2 GetPosition();
+	POINT GetPosition() { return pos; }
+};
+
