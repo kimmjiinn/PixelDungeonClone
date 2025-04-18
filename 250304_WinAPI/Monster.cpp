21bc804905633f9167cd52f568ce6caedb64086d
kimmjiinn <nankimminjin@gmail.com>
2025-04-19 00:23:50 +0900

    feat: monster
    렌더용 추가

--- 250304_WinAPI/Monster.cpp
+++ 250304_WinAPI/Monster.cpp
@@ -1,9 +1,13 @@
 ﻿#include "Monster.h"
 #include "Map.h"
 #include "Game.h"
+#include "Image.h"
 
 Monster::Monster()
 {
+    image = ImageManager::GetInstance()->AddImage("rat_Idle", L"Image/rat_Idle.bmp", 16 * 4, 16, 4, 1, true, RGB(255, 0, 255));
+    maxFrame = 4;
+    currFrame = 0;
 }
 
 void Monster::Move(int dx, int dy, Map map)
@@ -23,8 +27,14 @@ bool Monster::NeedInput()
 // 렌더용
 void Monster::Update()
 {
+    currFrame++;
+    if (currFrame >= maxFrame)
+    {
+        currFrame = 0;
+    }
 }
 
 void Monster::Render(HDC hdc)
 {
+    image->FrameRender(hdc, pos.x, pos.y, currFrame, 0);
 }
