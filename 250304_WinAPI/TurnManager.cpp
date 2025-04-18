56ae46f42275399d9c2f2712dab0863f65c5260f
kimmjiinn <nankimminjin@gmail.com>
2025-04-18 23:43:24 +0900

    feat: TurnManager class
    턴매니저 클래스 추가

---
+++ 250304_WinAPI/TurnManager.cpp
@@ -0,0 +1,23 @@
+﻿#include "TurnManager.h"
+#include "Game.h"
+#include "Entity.h"
+
+void TurnManager::AddActor(Entity* actor)
+{
+    turnQueue.push_back(actor); // 첫번째는 항상 플레이어
+}
+
+void TurnManager::ProcessTurns(Game game)
+{
+    if (turnQueue.empty())
+        return;
+}
+
+void TurnManager::EndTurn()
+{
+}
+
+Entity* TurnManager::GetCurrentActor()
+{
+    return nullptr;
+}
---
+++ 250304_WinAPI/TurnManager.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "config.h"
+
+class Entity;
+class Game;
+class TurnManager
+{
+public:
+	void AddActor(Entity *actor);
+	void ProcessTurns(Game game);
+	void EndTurn();
+	Entity* GetCurrentActor();
+private:
+	vector<Entity*> turnQueue;
+	int currentActorIndex;
+};
+
