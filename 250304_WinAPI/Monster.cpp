88f219765ed91e99bec7e321fce4b14a8470f4e8
kimmjiinn <nankimminjin@gmail.com>
2025-04-18 22:46:55 +0900

    feat: monster class
    몬스터 클래스 추가

---
+++ 250304_WinAPI/Monster.cpp
@@ -0,0 +1,28 @@
+﻿#include "Monster.h"
+#include "Map.h"
+#include "MainGame.h"
+
+Monster::Monster()
+{
+}
+
+void Monster::Move(int dx, int dy, Map map)
+{
+}
+
+void Monster::Act(MainGame)
+{
+}
+
+bool Monster::NeedInput()
+{
+    return false;
+}
+
+void Monster::Update()
+{
+}
+
+void Monster::Render(HDC hdc)
+{
+}
---
+++ 250304_WinAPI/Monster.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "Entity.h"
+
+class Map;
+class Monster : public Entity
+{
+public:
+	Monster();
+	virtual ~Monster() {};
+
+	void Move(int dx, int dy, Map map);
+	virtual void Act(MainGame) override;
+	virtual bool NeedInput() override;
+
+	// 렌더에 필요한 것들
+public:
+	void Update();
+	void Render(HDC hdc);
+
+private:
+	Image* image;
+	int maxFrame;
+	int currFrame;
+};
